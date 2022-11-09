e5cefbe1344216c16e9fdbe41a2a781bec8a7525
Kira <kshi.txt@yandex.ru>
2022-11-09 22:11:25 +0500

    рамка мемная готова, решить проблему с размерами и начинать регистрацию (для приличия можно решить трабл с текстом в рамке)

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ class Osnova(QMainWindow):
         self.bibl.clicked.connect(self.show_sbl)
         self.vst_text.clicked.connect(self.spros_text)
         self.vst_pic.clicked.connect(self.spros_pic)
+        self.ramka.clicked.connect(self.mem_zagolovok)
         self.sohranit.clicked.connect(self.sohr)
         self.flag_ok.clicked.connect(self.flgok)
         self.flag_ok.hide()
@@ -96,9 +97,9 @@ class Osnova(QMainWindow):
         nazv, ok = QInputDialog.getText(self, "Имя файла",
                                         "Как назвать файл?")
         try:
+            sizezz = 1000, 1000
+            self.img.resize(sizezz, Image.LANCZOS)
             if nazv:
-                sizezz = 1000, 1000
-                self.img.thumbnail(sizezz)
                 self.img.save(f'{nazv}.png')
             else:
                 self.img.save('Вы_не_ввели_название.png')
@@ -114,7 +115,7 @@ class Osnova(QMainWindow):
             self.fname = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '')[0]
             self.img = Image.open(self.fname)
             self.size = 400, 400
-            self.img.thumbnail(self.size, Image.Resampling.LANCZOS)
+            self.img.thumbnail(self.size, Image.LANCZOS)
             self.width, self.height = self.img.size
             self.a = ImageQt(self.img)
             self.pixmap = QPixmap.fromImage(self.a)
@@ -155,11 +156,11 @@ class Osnova(QMainWindow):
                 self.font = ImageFont.truetype(f'{self.shrft}.ttf', size=self.size_text)
                 line_height = sum(self.font.getmetrics())
                 fontimage = Image.new('L', (self.font.getsize(name)[0], line_height))
+                x, y = fontimage.size
                 ImageDraw.Draw(fontimage).text((0, 0), name, fill=255, font=self.font)
-                self.draw_text = ImageDraw.Draw(self.nazd)
                 fontimage = fontimage.rotate(self.grds_txt, resample=Image.BICUBIC, expand=True)
-                x = self.x1 - 30
-                y = self.y1 - 30 - ((400 - self.height) // 2)
+                x = self.x1 - 30 - (x // 2)
+                y = self.y1 - 30 - ((400 - self.height) // 2) - (y // 2)
                 self.flag_ok.show()
                 self.sohranit.hide()
                 for i in range(3):
@@ -218,7 +219,7 @@ class Osnova(QMainWindow):
             self.nazd = self.img.copy()
             self.pic = Image.open(self.fname_1)
             self.size_1 = self.size_pic, self.size_pic
-            self.pic.thumbnail(self.size_1)
+            self.pic.thumbnail(self.size_1, Image.LANCZOS)
             self.pic = self.pic.rotate(self.grds_pic, resample=Image.Resampling.BICUBIC, expand=True,
                                        fillcolor=(0, 0, 255))
             rgba = self.pic.convert("RGBA")
@@ -244,6 +245,54 @@ class Osnova(QMainWindow):
             self.label.setPixmap(self.pixmap)
             self.flag_1 = False
             self.flag_deistv = False
+        except:
+            _translate = QtCore.QCoreApplication.translate
+            self.label.setText(_translate("MainWindow",
+                                          "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; \
+                                           font-weight:600;\">Вы не выбрали картинку!</span></p></body></html>"))
+
+    def mem_zagolovok(self):
+        if self.flag_deistv:
+            self.zag_ramk, self.oye = QInputDialog.getText(self, "Введите заголовок", "пиши")
+            self.mem_text()
+        else:
+            QMessageBox.critical(self, "Ой, ошибка ", "Ты забыл закончить предыдущее действие, бака", QMessageBox.Ok)
+
+    def mem_text(self):
+        if self.flag_deistv:
+            self.text_ramk, self.oey = QInputDialog.getText(self, "Текст", "Введите текст")
+            self.mem_ramka()
+        else:
+            QMessageBox.critical(self, "Ой, ошибка ", "Ты забыл закончить предыдущее действие, бака", QMessageBox.Ok)
+
+    def mem_ramka(self):
+        try:
+            self.ramka = Image.new('RGBA', (self.width + 50, self.height + 150), 'black')
+            idraw = ImageDraw.Draw(self.ramka)
+            idraw.rectangle((20, 20, self.width + 30, self.height + 30), fill='white')
+            idraw.rectangle((23, 23, self.width + 27, self.height + 27), fill='black')
+
+            font = ImageFont.truetype(f'Times_new_roman.ttf', size=50)
+            line_height1 = sum(font.getmetrics())
+            fontimage1 = Image.new('L', (font.getsize(self.zag_ramk)[0], line_height1))
+            x, y = fontimage1.size
+            ImageDraw.Draw(fontimage1).text((0, 0), self.zag_ramk, fill=255, font=font)
+            self.ramka.paste((255, 255, 255), ((self.width + 50 - x) // 2, (self.height + 40)),
+                             mask=fontimage1)
+
+            font1 = ImageFont.truetype(f'Arial.ttf', size=20)
+            line_height = sum(font1.getmetrics())
+            fontimage = Image.new('L', (font1.getsize(self.text_ramk)[0], line_height))
+            x, y = fontimage.size
+            ImageDraw.Draw(fontimage).text((0, 0), self.text_ramk, fill=255, font=font1)
+            self.ramka.paste((255, 255, 255), ((self.width + 50 - x) // 2, (self.height + 130 - y)),
+                             mask=fontimage)
+            self.ramka.paste(self.img, (25, 25))
+            self.ramka.thumbnail(self.size, Image.LANCZOS)
+            self.img = self.ramka.copy()
+            self.a = ImageQt(self.img)
+            self.pixmap = QPixmap.fromImage(self.a)
+            self.label.setPixmap(self.pixmap)
         except:
             _translate = QtCore.QCoreApplication.translate
             self.label.setText(_translate("MainWindow",
